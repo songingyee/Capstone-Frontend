1824f5a9d412baa0915d1d2fe5ca963a6cb8083f
kim020702 <152774593+kim020702@users.noreply.github.com>
2024-06-01 15:25:43 +0900

    Update women.js

--- women.js
+++ women.js
@@ -1,7 +1,8 @@
+
 async function loadProducts() {
     try {
         // 목 서버에서 데이터를 받아옵니다.
-        const response = await fetch('https://bbadb468-58ad-46f5-80b5-69823ad76816.mock.pstmn.io/mock');
+        const response = await fetch('http://localhost:8080/api/women?page=X페이지');
         const data = await response.json();
 
         // 받아온 데이터를 화면에 추가합니다.
@@ -12,6 +13,7 @@ async function loadProducts() {
         data.data.forEach(product => { // 데이터는 'data' 속성에 있습니다.
             const listItem = document.createElement('li');
             listItem.id = `product${product.id}`;
+
             listItem.innerHTML = `
                 <div class="img" style="position: relative;">
                     <button type="button" name="button" class="heart" onclick="toggleButton('${listItem.id}', this)"></button>
@@ -19,15 +21,18 @@ async function loadProducts() {
                         <img src="${product.image}" alt="Product Image" class="productimg">
                 </div>
                 <div class="spacer"></div>
-                <div class="text max">
+                <div class="text-max">
                     <div class="text_wrap">
                         <div class="brand">${product.company}</div>
                     </div>
+                    <div class="spacer1"></div>
+                    <div class="size">${product.size}</div>
                     <div class="spacer1"></div>
                     <div class="manual">${product.itemName}</div>
                     <div class="spacer1"></div>
                     <div class="price">
                         <span class="base_price">${product.price}원</span>
+                    <div class="spacer2"></div>
                     </div>
                 </div>
                 </a>
@@ -37,6 +42,13 @@ async function loadProducts() {
 
         // 페이지 로드시 버튼 상태 로드
         loadButtonStates();
+
+
+
+
+
+
+
     } catch (error) {
         console.error('Failed to load products:', error);
     }
