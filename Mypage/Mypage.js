f3d22dc257fb0dad8c8fe16e6f55082ff0b43a58
Ingyeong Song <zptpfktpfk19@gmail.com>
2024-06-02 21:40:35 +0900

    Update Mypage.js

--- Mypage/Mypage.js
+++ Mypage/Mypage.js
@@ -57,7 +57,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-// 쿠키에서 특정 이름의 쿠키 값을 가져오는 함수
+// 쿠키에서 특정 이름의 쿠키 값을 가져옴
 function getCookie(name) {
   const cookieValue = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
   return cookieValue ? cookieValue.pop() : '';
